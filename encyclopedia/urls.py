aece4c293234a10477b9a0200402892adce2bb18
Rodrigo Casanova <rodrigocralha@gmail.com>
2022-07-05 18:13:40 +0100

    entry

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -3,5 +3,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("<str:title>/", views.entry, name="entry"),
+    path("search/", views.search, name="search"),
+    path("newPage/", views.newPage, name="newPage"),
+    path("editPage/", views.editPage, name="editPage"),
+    path("randomPage", views.randomPage, name="randomPage"),
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,10 +1,41 @@
 from django.shortcuts import render
+from markdown2 import *
 
 from . import util
 
+def convert_to_html(title):
+    entry = util.get_entry(title)
+
+    if entry is None:
+        return None
+    else:
+        return markdown(entry)
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
 
+def entry(request, title):
+    print(util.get_entry(title))
+    return render(request, "encyclopedia/entry.html", {
+        "title": title,
+        "entry": convert_to_html(title)
+    })
+
+def search(request):
+    query = request.GET.get("q")
+    return render(request, "encyclopedia/search.html", {
+        "entries": util.search(query)
+    })
+
+def newPage(request):
+    return render(request, "encyclopedia/newPage.html")
+
+def editPage(request):
+    return render(request, "encyclopedia/editPage.html")
+
+def randomPage(request):
+    return render(request, "encyclopedia/randomPage.html", {
+        "entry": util.get_random_entry()
+    })
